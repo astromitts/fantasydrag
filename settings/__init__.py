c516af0a11758be37bc90510300b90c75dd2484c
Bo Morin <morinbe@gmail.com>
2021-06-24 06:20:22 -0400

    Add secrets and move prod key out of it

--- settings/__init__.py
+++ settings/__init__.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u#!h-&b9t@$^#qu1&+&)ij)1q!dl07g=2x)v!not&oggk1%!6w'
+SECRET_KEY = 'yourawinnerbaby'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [''],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
